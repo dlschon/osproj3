3405021977ca7dd27d43caa00201c6239bfee7c7
dlschon <dlschon0@gmail.com>
2018-11-08 02:33:07 -0600

    added zfilez

--- zfilez.c
+++ zfilez.c
@@ -2,5 +2,12 @@
 
 int main(int argc, char** argv) 
 {
+  // Fetch the key environment vars
+  char cwd[MAX_PATH_LENGTH];
+  char disk_name[MAX_PATH_LENGTH];
+  oufs_get_environment(cwd, disk_name);
+  
+  oufs_list(cwd, disk_name);
+
   return 0;
 }
