7f576338db414ae059ec481e95cacfe2fcdc4ae4
dlschon <dlschon0@gmail.com>
2018-11-08 02:45:21 -0600

    fixed name

--- zfilez.c
+++ zfilez.c
@@ -7,12 +7,7 @@ int main(int argc, char** argv)
   char disk_name[MAX_PATH_LENGTH];
   oufs_get_environment(cwd, disk_name);
   
-  // Open virtual disk
-  vdisk_disk_open(virtual_disk_name);
-
   oufs_list("/", "/");
-
-  vdisk_disk_close();
 
   return 0;
 }
